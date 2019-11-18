ae9aae4f29587ee637a39cae054ff6131d16a95d
white-958 <kostascool.cool@gmail.com>
2019-11-18 22:24:54 +0200

    Main changes

--- Example/src/main/java/Main_Test.java
+++ Example/src/main/java/Main_Test.java
@@ -3,5 +3,6 @@ public class Main_Test {
     public static void main(String[] args){
         System.out.println("Whatttt ???? you never played....");
         System.out.println("Nooo F you maaan ....");
+        System.out.println("What the F this means ?????");
     }
 }
